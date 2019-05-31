5fe49e8c06ab2a34143461fe5579e64c031f0c42
John <johnarpaul@gmail.com>
2019-05-31 16:14:16 +0100

    added user type enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum user_type: [:student, :teacher, :admin]
 end
