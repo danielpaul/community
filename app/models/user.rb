4576a62165cdb2573ec473148273526971ac7745
John <johnarpaul@gmail.com>
2019-06-04 12:09:10 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum user_type: [:student, :teacher, :admin]
+
+  profanity_filter! :display_name, :method => 'dictionary'
+  #=> banned words will be replaced by value in config/dictionary.yml
 end
