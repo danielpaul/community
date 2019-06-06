8e076c7d0a77927ce557f7637278cdc310e481dd
John <johnarpaul@gmail.com>
2019-06-06 13:41:03 +0100

    made devise omni auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :trackable
+         :confirmable, :trackable,
+         :omniauthable, omniauth_providers: %i[facebook]
 
   attr_accessor :school_year
 
