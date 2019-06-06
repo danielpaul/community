445111ccd87b4a72342047fe8b36f547d2b78594
John <johnarpaul@gmail.com>
2019-06-06 13:41:45 +0100

    modified users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_151012) do
+ActiveRecord::Schema.define(version: 2019_06_06_124112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_06_04_151012) do
     t.integer "user_type"
     t.integer "year_of_graduation"
     t.boolean "ty"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
