9b1960acf9999446a10cd89033300c6b88c7c8ea
John <johnarpaul@gmail.com>
2019-06-12 17:13:56 +0100

    created category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_160547) do
+ActiveRecord::Schema.define(version: 2019_06_12_161002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_06_12_160547) do
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.bigint "categories_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["categories_id"], name: "index_categories_on_categories_id"
   end
 
