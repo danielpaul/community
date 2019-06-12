fb338355eb8a8a02f797c1558e2f2fcd962a9969
John <johnarpaul@gmail.com>
2019-06-12 16:52:35 +0100

    user controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,9 +6,11 @@ RSpec.describe UsersController, type: :controller do
   render_views
 
   #------- FACTORY -------#
+
   let(:user) { create(:user) }
 
   #------- METHODS -------#
+
   def get_show
     get :show, params: { id: user }
   end
@@ -23,13 +25,58 @@ RSpec.describe UsersController, type: :controller do
 
   #------- TESTS -------#
 
-  before(:each) do
-    sign_in user
-  end
-
   describe "GET #index" do
     it "directs to index" do
-      expect(get :index).to render_template("index")
+      sign_in user
+      expect(get :index).to redirect_to user_path(user.id)
+    end
+  end
+
+  describe "GET #show" do
+    before(:each) do
+      get_show
+      sign_in user
+    end
+
+    it "assigns the requested user to @user" do
+      expect(assigns(:user)).to eq(user)
+    end
+
+    it "renders show with success" do
+      expect(response).to render_template("show")
+    end
+  end
+
+  describe "PUT #update" do
+    context "logged out user" do
+      it "redirects to user sign in" do
+        debugger
+        expect(put_update).to redirect_to new_user_session_path
+      end
+    end
+
+    context "logged in user" do
+      it "allows update" do
+        sign_in user
+        expect(User.find(user.id).first_name).to eq('John')
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    context "logged out user" do
+      it "redirects to user sign in" do
+        expect(delete_destroy).to redirect_to new_user_session_path
+      end
+    end
+
+    context "logged in user" do
+      it "deletes the user" do
+        sign_in user
+        expect {
+          delete_destroy
+        }.to change(User, :count).by(-1)
+      end
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require 'rspec/rails'
 require 'support/factory_bot'
 
 require 'faker'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
