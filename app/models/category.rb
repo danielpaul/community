431693c66bf7528b00da0c134a01ccb99e3032b8
John <johnarpaul@gmail.com>
2019-06-12 17:20:42 +0100

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,10 @@
 class Category < ApplicationRecord
+
+  #------- PLUGINS -------#
   extend FriendlyId
   friendly_id :name, use: :slugged
+
+  #------- VALIDATIONS -------#
+  validates :name, presence: true
+
 end
