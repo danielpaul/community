bffaa074f0add02ace01f247c65e4de93c5bb81a
John <johnarpaul@gmail.com>
2019-06-13 13:23:17 +0100

    added posts controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+
+  # ---------- [ Columns ] ---------- #
+  enum status: { published: 0, draft: 1 }
+  enum publicity: { public: 0, unlisted: 1, private: 2 }
+
+  #------- PLUGINS -------#
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  #------- VALIDATIONS -------#
+  validates :title, :status, :publicity, presence: true
+
 end
